c2d0ead5337ff6950bb2eb9534fa79e95665a4bf
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-02-20 17:26:28 +0530

    client.cpp: fix: Replace the structure variable in the inet_pton() which will IPv4 from text to binary form

--- client.cpp
+++ client.cpp
@@ -13,24 +13,26 @@ int main()
     net_socket = socket(AF_INET,SOCK_STREAM,0);
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
+    int addrlen = sizeof(addr);
     addr.sin_port = htons(9090);
     if (net_socket<0)
-    {   cout<<"failure"<<endl;
+    {   perror("socket");
         exit(1);
     }
     cout<<"Socket created successfully"<<endl;
 
-    if (inet_pton(AF_INET,"0.0.0.0",&addr.sin_port)<=0)
+    if (inet_pton(AF_INET,"0.0.0.0",&addr.sin_addr)<=0)
     {
         cout<<"Invalid address"<<endl;
         return -1;
     }
     cout<<"Valid address"<<endl;
-
-    if (connect(net_socket, (struct sockaddr *) &addr, sizeof(addr))<0)
+    
+    int connect_var = connect(net_socket, (struct sockaddr *) &addr, addrlen);
+    if (connect_var<0)
     {
-        cout<<"Connection failed"<<endl;
-        return -1;
+        perror("connection");
+        exit(1);
     }
     cin.getline(buffer,1024);
     send(net_socket,buffer,1024,0);
